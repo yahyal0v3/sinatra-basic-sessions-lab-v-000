374654a0b55dd9fa5017a4a57ee684e95bfd8a98
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-17 02:42:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :session
+    set :session_secret, 'secret'
+  end
+  
   get '/' do
     erb :index
   end
